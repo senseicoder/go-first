6ada9bc51d594fee8981133c702905e99c53c301
Cédric Girard <c.girard@epiconcept.fr>
2025-05-22 15:45:15 +0200

    correction typo

--- namecheck/main.go
+++ namecheck/main.go
@@ -29,7 +29,7 @@ func main() {
 				fmt.Println("Error: ", err)
 			}
 			if res {
-				fmt.Println("github: ", firstArg)
+				fmt.Println(network.Name(), ": ", firstArg)
 				fmt.Println(network.IsAvailable(firstArg))
 			}
 		}
